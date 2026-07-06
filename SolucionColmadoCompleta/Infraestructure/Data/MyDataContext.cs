7592204b8b69dc4469641a167f0344575a9d84a6
josemiliriano <josemiliriano83@gmail.com>
2026-07-06 19:25:36 -0400

    Conexion a la base de datos

---
+++ SolucionColmadoCompleta/Infraestructure/Data/MyDataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infraestructure.Data
+{
+    public class MyDataContext: DbContext
+    {
+        public MyDataContext( DbContextOptions<MyDataContext>options):base(options)
+        {
+
+        }
+    }
+}
