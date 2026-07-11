9bbc73f1702dcfb9989b586707da79073fd19992
josemiliriano <josemiliriano83@gmail.com>
2026-07-11 16:06:04 -0400

    Creacion de la interfaz y el servicio de categoria

---
+++ SolucionColmadoCompleta/Application/Contract/ICategoryAppService.cs
@@ -0,0 +1,20 @@
+﻿using Application.DTOs;
+using Domain.Entities;
+using Infraestructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Contract
+{
+    public interface ICategoryAppService
+    {
+        public CDCategory AddCategory(CategoryDto dto);
+        public List<CDCategory> GetAllCategory();
+        public CDCategory GetCategoryById(int id);
+        public CDCategory UpdateCategory(int id, CategoryDto dto);
+        public void DeleteCategory(int id);
+        public bool SoftDelete(int id);
+        public List<CDCategory> GetCategoryNotDeleted();
+    }
+}
