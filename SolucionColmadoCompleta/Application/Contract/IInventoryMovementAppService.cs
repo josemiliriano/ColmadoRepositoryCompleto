1e9d944cb16e96735bad13d4e5465b85d8654ac1
josemiliriano <josemiliriano83@gmail.com>
2026-07-12 14:16:15 -0400

    Creacion de Interfaz y Servicio de los movimientos de inventario

---
+++ SolucionColmadoCompleta/Application/Contract/IInventoryMovementAppService.cs
@@ -0,0 +1,22 @@
+﻿using Application.DTOs;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Contract
+{
+    public interface IInventoryMovementAppService
+    {
+        public CDInventoryMovement AddBuy(InventoryMovementDto Buy);
+        public CDInventoryMovement AddSale(InventoryMovementDto Sale);
+        public List<CDInventoryMovement> GetAllBuy();
+        public List<CDInventoryMovement> GetAllSale();
+        public CDInventoryMovement GetById(int id);
+        public CDInventoryMovement UpdateInventary(int id, InventoryMovementDto movement);
+        public void DeleteTrasaction(int id);
+        public bool SoftDelete(int id);
+        public List<CDInventoryMovement> GetAllTransactionWihtCondition();
+        public List<TrasactionCompleteDto> GetAllTransactionComplete();
+    }
+}
---
+++ SolucionColmadoCompleta/Application/DTOs/TrasactionCompleteDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs
+{
+    public class TrasactionCompleteDto
+    {
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public string ProviderName { get; set; }
+        public string DescriptionMovement { get; set; }
+    }
+}
